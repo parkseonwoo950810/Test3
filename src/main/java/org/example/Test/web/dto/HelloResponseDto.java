473c2bae5e2c3f3082ed2d15e6d03eeb1c189426
parkseonwoo950810 <bswoo0714@naver.com>
2020-09-28 11:55:59 +0900

    gitignore 등록

---
+++ src/main/java/org/example/Test/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package org.example.Test.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
