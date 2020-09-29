36e3cc10658f0bb7ff086bf0a594b552417e1ea1
parkseonwoo950810 <bswoo0714@naver.com>
2020-09-29 10:01:00 +0900

    gitignore 등록

--- src/main/java/org/example/Test/service/posts/PostsService.java
+++ src/main/java/org/example/Test/service/posts/PostsService.java
@@ -1,10 +1,18 @@
 package org.example.Test.service.posts;
 
+import org.example.Test.domain.posts.Posts;
+import org.example.Test.domain.posts.PostsRepository;
+import org.example.Test.web.dto.*;
+
 import lombok.RequiredArgsConstructor;
 import org.example.Test.domain.posts.PostsRepository;
 import org.example.Test.web.dto.PostsSaveRequestDto;
+import org.example.Test.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
 
 @RequiredArgsConstructor
 @Service
@@ -16,4 +24,39 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    /*
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+    */
 }
--- src/main/java/org/example/Test/web/PostsApiController.java
+++ src/main/java/org/example/Test/web/PostsApiController.java
@@ -1,11 +1,18 @@
 package org.example.Test.web;
 
-import lombok.RequiredArgsConstructor;
 import org.example.Test.service.posts.PostsService;
-import org.example.Test.web.dto.PostsSaveRequestDto;
+import org.example.Test.web.dto.*;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,4 +24,27 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    /*
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+     */
 }
