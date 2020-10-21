7743f9165e24e271f4cd802a5be894d3d804bb73
parkseonwoo950810 <bswoo0714@naver.com>
2020-10-21 18:00:38 +0900

    gitignore 등록

--- src/test/java/org/example/Test/web/HelloControllerTest.java
+++ src/test/java/org/example/Test/web/HelloControllerTest.java
@@ -3,7 +3,7 @@ package org.example.Test.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,15 +14,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
-    /*
     @Test
-    public void hello_return() throws Exception {
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello"))
@@ -31,19 +29,16 @@ public class HelloControllerTest {
     }
 
     @Test
-    public void helloDto_return() throws Exception {
+    public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
 
         mvc.perform(
-                    get("/hello/dto")
-                                    .param("name", name)
-                                    .param("amount", String.valueOf(amount)))
-                                    .andExpect(status().isOk())
-                                    .andExpect(jsonPath("$.name", is(name)))
-                                    .andExpect(jsonPath("$.amount", is(amount)));
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
-
-     */
 }
-
